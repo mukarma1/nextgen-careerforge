ed0224c6551539c003ed71f0be7b6e422ef2c373
Mukarmah <mukarmarehman@gmail.com>
2026-05-23 22:06:25 +0500

    Setup React Router and created ResumeAudit page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import Home from './pages/Home' // Humne Home page ko bulaya
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import ResumeAudit from './pages/ResumeAudit' // Hum ye page abhi banayein ge
 
 function App() {
   return (
-    <div>
-      <Home /> {/* Home page ko yahan dikhaya */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/audit" element={<ResumeAudit />} />
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Features from '../components/Features'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
@@ -14,7 +15,7 @@ function Home() {
         <div className="absolute top-20 left-1/2 -translate-x-1/2 w-72 h-72 bg-blue-600 rounded-full blur-[120px] opacity-30 -z-10"></div>
         
         <h1 className="text-5xl md:text-8xl font-black mb-6 leading-tight">
-          Master Your <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-500">Big Tech</span> Journey
+          Master Your Big Tech <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-500"></span> Journey
         </h1>
         
         <p className="text-gray-400 text-lg md:text-xl max-w-3xl mb-10 leading-relaxed">
@@ -22,10 +23,12 @@ function Home() {
         </p>
 
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
+         <Link to="/audit">
           <button className="px-8 py-4 bg-blue-600 hover:bg-blue-700 rounded-xl font-bold text-lg transition-all transform hover:scale-105 shadow-xl">
             Audit My Resume
           </button>
-          <button className="px-8 py-4 bg-slate-800 hover:bg-slate-700 border border-slate-700 rounded-xl font-bold text-lg transition-all">
+          </Link>
+          <button className="px-8 py-4 bg-blue-600 hover:bg-blue-700 rounded-xl font-bold text-lg transition-all transform hover:scale-105 shadow-xl">
             Track DSA Progress
           </button>
         </div>
@@ -36,7 +39,7 @@ function Home() {
 
       {/* Simple Footer */}
       <footer className="py-10 text-center text-gray-500 text-sm border-t border-slate-800 mt-20">
-        © 2024 NexGen CareerForge. Built with Courage by a Pakistani Developer.
+        © 2026 NexGen CareerForge. Built with Courage by a Pakistani Developer.
       </footer>
     </div>
   )
